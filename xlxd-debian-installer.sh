a95d97c9bd7f4685294bf0f8796e4c0afff39e68
Daniel Kondlatsch <daniel.kondlatsch@gmail.com>
2024-06-13 20:14:08 -0300

    Update xlxd-debian-installer.sh
    New file

--- xlxd-debian-installer.sh
+++ xlxd-debian-installer.sh
@@ -1,156 +1,150 @@
 #!/bin/bash
-# A tool to install xlxd, your own D-Star Reflector.
+# A tool to install XLXD, your own D-Star Reflector.
 # For more information, please visit: https://n5amd.com
-#Lets begin-------------------------------------------------------------------------------------------------
-WHO=$(whoami)
-if [ "$WHO" != "root" ]
-then
-  echo ""
-  echo "You Must be root to run this script!!"
-  exit 0
+# Customized by Daniel K., PU5KOD
+# Lets begin!!!
+
+# root user check
+if [ "$(whoami)" != "root" ]; then
+  echo "You must be root to run this script!"
+  exit 1
 fi
-if [ ! -e "/etc/debian_version" ]
-then
-  echo ""
-  echo "This script is only tested on Debian and x64 CPU Arch. but XLX will work on most ANY Linux distro. "
-  exit 0
+
+# Distro check
+if [ ! -e "/etc/debian_version" ]; then
+  echo "This script is only tested on Debian-based distributions."
+  exit 1
 fi
+
 DIRDIR=$(pwd)
 LOCAL_IP=$(hostname -I | awk '{print $1}')
-INFREF=https://n5amd.com/digital-radio-how-tos/create-xlx-xrf-d-star-reflector/
-XLXDREPO=https://github.com/LX3JL/xlxd.git
-DMRIDURL=http://xlxapi.rlx.lu/api/exportdmr.php
-WEBDIR=/var/www/xlxd
-XLXINSTDIR=/root/reflector-install-files/xlxd
-DEP="git build-essential apache2 php libapache2-mod-php php7.0-mbstring"
-DEP2="git build-essential apache2 php libapache2-mod-php php7.3-mbstring"
-DEP3="git build-essential apache2 php libapache2-mod-php php7.4-mbstring"
-APPS="git git-core apache2 php libapache2-mod-php php-cli php-xml php-mbstring php-curl build-essential"
-VERSION=$(sed 's/\..*//' /etc/debian_version)
+INFREF="https://n5amd.com/digital-radio-how-tos/create-xlx-xrf-d-star-reflector/"
+XLXDREPO="https://github.com/LX3JL/xlxd.git"
+DMRIDURL="http://xlxapi.rlx.lu/api/exportdmr.php"
+WEBDIR="/var/www/xlxd"
+XLXINSTDIR="/usr/src"
+APPS="git git-core apache2 php libapache2-mod-php php-cli php-xml php-mbstring php-curl build-essential g++"
+
+# DATA INPUT
 clear
+echo "HAM Radio Multiprotocol DStar Reflector Server"
+echo ""
+echo "REFLECTOR VARIABLE INPUT"
 echo ""
 echo "XLX uses 3 digit numbers for its reflectors. For example: 032, 999, 099."
 read -p "What 3 digit XRF number will you be using?  " XRFDIGIT
 XRFNUM=XLX$XRFDIGIT
-echo ""
-echo "--------------------------------------"
 read -p "What is the FQDN of the XLX Reflector dashboard? Example: xlx.domain.com.  " XLXDOMAIN
-echo ""
-echo "--------------------------------------"
 read -p "What E-Mail address can your users send questions to?  " EMAIL
-echo ""
-echo "--------------------------------------"
 read -p "What is the admins callsign?  " CALLSIGN
-echo ""
-echo ""
-echo "------------------------------------------------------------------------------"
-echo "Making install directories and installing dependicies...."
-echo "------------------------------------------------------------------------------"
-mkdir -p $XLXINSTDIR
-mkdir -p $WEBDIR
-apt-get -y update
-apt-get -y install $APPS
-#if [ $VERSION = 9 ]
-#then
-#    apt-get -y install $DEP
-#    a2enmod php7.0
-#elif [ $VERSION = 10 ]
-#then
-#    apt-get -y install $DEP2
-#elif [ $VERSION = 11 ]
-#then
-#    apt-get -y install $DEP3
-#fi
+read -p "What is the Country Reflector?  " COUNTRY
+read -p "What is the Reflector Comment to display on dashboard?  " COMMENT
+read -p "How many active modules does the reflector have? (1-26)  " MODQTD
+read -p "What is the number of YSF UDP port? (1-65535)  " YSFPORT
+read -p "What is the frequency of YSF Wires-X? (In Hertz, with 9 digits, ex. 433125000)  " YSFFREQ
+read -p "Is YSF Auto-Link enable? (1 = Yes / 0 = No)  " AUTOLINK
+VALID_MODULES=($(echo {A..Z} | cut -d' ' -f1-"$MODQTD"))
+if [ "$AUTOLINK" -eq 1 ]; then
+  while true; do
+    read -p "YSF module to Auto-Link? (one of ${VALID_MODULES[*]}): " MODAUTO
+    MODAUTO=$(echo "$MODAUTO" | tr '[:lower:]' '[:upper:]')
+    if [[ " ${VALID_MODULES[@]} " =~ " $MODAUTO " ]]; then
+      break
+    else
+      echo "Invalid input for YSF autolink module. Must be one of ${VALID_MODULES[*]}."
+    fi
+  done
+fi
 
-echo "------------------------------------------------------------------------------"
-if [ -e $XLXINSTDIR/xlxd/src/xlxd ]
-then
-   echo ""
-   echo "It looks like you have already compiled XLXD. If you want to install/complile xlxd again, delete the directory '/root/reflector-install-files/xlxd' and run this script again. "
-   exit 0
+echo ""
+echo "UPDATING OS..."
+echo "=============="
+echo ""
+
+mkdir -p "$XLXINSTDIR"
+mkdir -p "$WEBDIR"
+
+apt -y update
+apt -y upgrade
+apt -y install $APPS
+
+if [ -e "$XLXINSTDIR/xlxd/src/xlxd" ]; then
+  echo "XLXD already compiled. Delete the following directories"
+  echo "'/usr/src/xlxd', '/xlxd', '/var/www/xlxd' and the following files"
+  echo "'/etc/init.d/xlxd.*', 'var/log/xlxd.*' and '/etc/apache2/sites-available/xlx*'"
+  exit 1
 else
-   echo "Downloading and compiling xlxd... "
-   echo "------------------------------------------------------------------------------"
-   cd $XLXINSTDIR
-   git clone $XLXDREPO
-   cd $XLXINSTDIR/xlxd/src
-   make clean
-   make
-   make install
+  echo ""
+  echo "DOWNLOADING APPLICATION..."
+  echo "=========================="
+  echo ""
+  cd "$XLXINSTDIR"
+  git clone "$XLXDREPO"
+  cd "$XLXINSTDIR/xlxd/src"
+  make clean
+  MAINCONFIG="$XLXINSTDIR/xlxd/src/main.h"
+  sed -i "s/\(NB_OF_MODULES\s*\)\([0-9]*\)/\1$MODQTD/" "$MAINCONFIG"
+  sed -i "s/\(YSF_PORT\s*\)\([0-9]*\)/\1$YSFPORT/" "$MAINCONFIG"
+  sed -i "s/\(YSF_DEFAULT_NODE_TX_FREQ\s*\)\([0-9]*\)/\1$YSFFREQ/" "$MAINCONFIG"
+  sed -i "s/\(YSF_DEFAULT_NODE_RX_FREQ\s*\)\([0-9]*\)/\1$YSFFREQ/" "$MAINCONFIG"
+  sed -i "s/\(YSF_AUTOLINK_ENABLE\s*\)\([0-9]*\)/\1$AUTOLINK/" "$MAINCONFIG"
+  if [ "$AUTOLINK" -eq 1 ]; then
+    sed -i "s/\(YSF_AUTOLINK_MODULE\s*\)'\([A-Z]*\)'/\1'$MODAUTO'/" "$MAINCONFIG"
+  fi
+  echo ""
+  echo "COMPILING..."
+  echo "============"
+  echo ""
+  make
+  make install
 fi
-if [ -e $XLXINSTDIR/xlxd/src/xlxd ]
-then
-   echo ""
-   echo ""
-   echo "------------------------------------------------------------------------------"
-   echo "It looks like everything compiled successfully. There is a 'xlxd' application file. "
+
+if [ -e "$XLXINSTDIR/xlxd/src/xlxd" ]; then
+  echo "============================="
+  echo "| COMPILATION SUCCESSFUL!!! |"
+  echo "============================="
 else
-   echo ""
-   echo "UH OH!! I dont see the xlxd application file after attempting to compile."
-   echo "The output above is the only indication as to why it might have failed.  "
-   echo "Delete the directory '/root/reflector-install-files/xlxd' and run this script again. "
-   echo ""
-   exit 0
+  echo "===================================================="
+  echo "| Compilation FAILED. Check the output for errors. |"
+  echo "===================================================="
+  exit 1
 fi
-echo "------------------------------------------------------------------------------"
-echo "Getting the DMRID.dat file... "
-echo "------------------------------------------------------------------------------"
-wget -O /xlxd/dmrid.dat $DMRIDURL
-echo "------------------------------------------------------------------------------"
-echo "Copying web dashboard files and updating init script... "
-cp -R $XLXINSTDIR/xlxd/dashboard/* /var/www/xlxd/
-cp $XLXINSTDIR/xlxd/scripts/xlxd /etc/init.d/xlxd
+
+mkdir -p /xlxd
+wget -O /xlxd/dmrid.dat "$DMRIDURL"
+
+cp -R "$XLXINSTDIR/xlxd/dashboard/"* "$WEBDIR/"
+cp "$XLXINSTDIR/xlxd/scripts/xlxd" /etc/init.d/xlxd
 sed -i "s/XLX999 192.168.1.240 127.0.0.1/$XRFNUM $LOCAL_IP 127.0.0.1/g" /etc/init.d/xlxd
 update-rc.d xlxd defaults
-# Delaying startup time
-# mv /etc/rc3.d/S01xlxd /etc/rc3.d/S10xlxd ##Disabling as its not really needed. 
-echo "Updating XLXD Config file... "
-XLXCONFIG=/var/www/xlxd/pgs/config.inc.php
-sed -i "s/your_email/$EMAIL/g" $XLXCONFIG
-sed -i "s/LX1IQ/$CALLSIGN/g" $XLXCONFIG
-sed -i "s/http:\/\/your_dashboard/http:\/\/$XLXDOMAIN/g" $XLXCONFIG
-sed -i "s/\/tmp\/callinghome.php/\/xlxd\/callinghome.php/g" $XLXCONFIG
-echo "Copying directives and reloading apache... "
-cp $DIRDIR/templates/apache.tbd.conf /etc/apache2/sites-available/$XLXDOMAIN.conf
-sed -i "s/apache.tbd/$XLXDOMAIN/g" /etc/apache2/sites-available/$XLXDOMAIN.conf
-sed -i "s/ysf-xlxd/xlxd/g" /etc/apache2/sites-available/$XLXDOMAIN.conf
-chown -R www-data:www-data /var/www/xlxd/
+
+XLXCONFIG="$WEBDIR/pgs/config.inc.php"
+sed -i "s/your_email/$EMAIL/g" "$XLXCONFIG"
+sed -i "s/LX1IQ/$CALLSIGN/g" "$XLXCONFIG"
+sed -i "s#http://your_dashboard#http://$XLXDOMAIN#g" "$XLXCONFIG"
+sed -i "s/your_country/$COUNTRY/g" "$XLXCONFIG"
+sed -i "s/your_comment/$COMMENT/g" "$XLXCONFIG"
+sed -i "s#/tmp/callinghome.php#/xlxd/callinghome.php#g" "$XLXCONFIG"
+sed -i "s#/tmp/lastcallhome.php#/xlxd/lastcallhome.php#g" "$XLXCONFIG"
+
+cp "$DIRDIR/templates/apache.tbd.conf" /etc/apache2/sites-available/"$XLXDOMAIN".conf
+sed -i "s/apache.tbd/$XLXDOMAIN/g" /etc/apache2/sites-available/"$XLXDOMAIN".conf
+sed -i "s/ysf-xlxd/html/g" /etc/apache2/sites-available/"$XLXDOMAIN".conf
+
+chown -R www-data:www-data "$WEBDIR/"
+chown -R www-data:www-data /var/www/html/
 chown -R www-data:www-data /xlxd/
-a2ensite $XLXDOMAIN
+
+a2ensite "$XLXDOMAIN"
 a2dissite 000-default
-service xlxd start
 systemctl restart apache2
-echo "------------------------------------------------------------------------------"
-echo ""
-echo ""
-echo "******************************************************************************"
-echo ""
-echo ""
-echo "XLXD is finished installing and ready to be used. Please read the following..."
-echo ""
-echo ""
-echo "******************************************************************************"
-echo ""
-echo " For Public Reflectors: "
-echo "If your XLX number is not already taken, enabling callinghome is all you need to do  "
-echo "for your reflector to be added to all the host files automatically. It does take     "
-echo "about an hour for the change to reflect, if your reflector is accessible and working."
-echo "Once activated, the callinghome hash to backup will be /xlxd/callinghome.php. "
+systemctl start xlxd
+
+echo "========================================================================================="
+echo "XLXD is finished installing and ready to be used."
+echo "For Public Reflectors:"
+echo "If your XLX number is not already taken, enabling callinghome is all you need to do."
 echo "More Information: $INFREF"
-echo ""
-echo ""
-echo " For test/private Reflectors: "
-echo "If you are using this reflector as a test or for offline access you will  "
-echo "need to configure the host files of the devices connecting to this server."
-echo "There are many online tutorials on 'Editing pi-star host files'.          "
-echo ""
-echo ""
-echo "          Your $XRFNUM dashboad should now be accessible...            "
-echo "                http://$XLXDOMAIN                                      "
-echo ""
-echo ""
+echo "Your $XRFNUM dashboard should now be accessible at http://$XLXDOMAIN"
 echo "You can make further customizations to the main config file $XLXCONFIG."
-echo "Be sure to thank the creators of xlxd for the ability to spin up          "
-echo "your very own D-Star reflector.                                           "
-echo ""
-echo "------------------------------------------------------------------------------"
+echo "========================================================================================="
