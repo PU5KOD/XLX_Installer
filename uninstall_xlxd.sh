8c32241934cdbb1ef0922026792934d502d03384
Daniel Kondlatsch <daniel.kondlatsch@gmail.com>
2024-06-16 15:11:13 -0300

    Update uninstall_xlxd.sh
    correcao no caminho do diretorio do dashboard

--- uninstall_xlxd.sh
+++ uninstall_xlxd.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo rm -r /usr/src/xlxd/ /xlxd/ /var/www/xlxd/
+sudo rm -r /usr/src/xlxd/ /xlxd/ /var/www/html/xlxd/
 sudo rm /etc/init.d/xlxd /var/log/xlxd.* /etc/apache2/sites-available/xlx*.*
