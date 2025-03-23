802353b626da1c5f7fead19dcbb3448fad28695b
Daniel Kondlatsch <daniel.kondlatsch@gmail.com>
2025-03-23 19:24:51 -0300

    Update uninstall_xlxd.sh

--- uninstall_xlxd.sh
+++ uninstall_xlxd.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+sudo systemctl stop xlxd.service
+sudo systemctl disable xlxd.service
 sudo rm -r /usr/src/xlxd/ /xlxd/ /var/www/html/xlxd/
 sudo rm /etc/init.d/xlxd /var/log/xlxd.* /etc/apache2/sites-available/xlx*.*
+sudo systemctl daemon-reload
