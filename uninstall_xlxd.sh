88a671ae361e1499aab8822b9b0dc8a638f8c252
Daniel Kondlatsch <daniel.kondlatsch@gmail.com>
2025-03-26 16:08:22 -0300

    Update uninstall_xlxd.sh
    updates

--- uninstall_xlxd.sh
+++ uninstall_xlxd.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+echo "XLX Uninstaller"
+echo ""
+read -r -p "What is the web address (FQDN) of the reflector dashboard? " XLXDOMAIN
 sudo systemctl stop xlxd.service
 sudo systemctl disable xlxd.service
+/usr/sbin/a2dissite $XLXDOMAIN.conf
+/usr/sbin/a2ensite 000-default
 sudo rm -r /usr/src/xlxd/ /xlxd/ /var/www/html/xlxd/
-sudo rm /etc/init.d/xlxd /var/log/xlxd.* /etc/apache2/sites-available/xlx*.*
+sudo rm /etc/init.d/xlxd /var/log/xlxd.* /etc/apache2/sites-available/$XLXDOMAIN
 sudo systemctl daemon-reload
