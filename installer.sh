81066858acab641743fb9c20923330cc66e40c02
Daniel Kondlatsch <daniel.kondlatsch@gmail.com>
2025-05-29 13:55:52 -0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -538,7 +538,6 @@ find "$WEBDIR" -type d -exec chmod 755 {} \;
 find "$WEBDIR" -type f -exec chmod 644 {} \;
 chmod 755 /xlxd/users_db/create_user_db.php
 chmod 755 /xlxd/users_db/update_db.sh
-chmod 644 /xlxd/users_db/user*
 chmod 644 /xlxd/xlxd.*
 chmod 755 /xlxd/xlxd
 chmod 755 /xlxd/xlxecho
